d357d4dfbde98d76f4947ab7ccf937b5085cdc9d
rakhmanhanna <rakhmanhanna@gmail.com>
2021-12-08 17:00:02 +0300

    task 1 edited

--- tasks.js
+++ tasks.js
@@ -3,9 +3,6 @@
 const marks = [50, 20, 70, 60, 45, 30];
 
 function findMin(arr) {
-  if (arr.length === 0) {
-    return undefined;
-  }
   let min = arr[0];
   arr.forEach((number) => {
     if (number < min) {
